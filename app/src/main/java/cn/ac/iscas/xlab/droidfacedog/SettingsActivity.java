353d49f0e68a99b302eb7ab6c1b6b90c5b17d28c
Wei Wu <lazyparser@gmail.com>
2016-10-19 14:34:21 +0800

    ui update

--- app/src/main/java/cn/ac/iscas/xlab/droidfacedog/SettingsActivity.java
+++ app/src/main/java/cn/ac/iscas/xlab/droidfacedog/SettingsActivity.java
@@ -189,7 +189,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
+            bindPreferenceSummaryToValue(findPreference("server_ip_address"));
             bindPreferenceSummaryToValue(findPreference("example_list"));
         }
 
