0a9d1a7e1510dfe6777ce23d70beced915253ccd
Lee <501648152@qq.com>
2017-05-10 13:34:21 +0800

    增加全局配置管理类Config

---
+++ app/src/main/java/cn/ac/iscas/xlab/droidfacedog/config/Config.java
@@ -0,0 +1,21 @@
+package cn.ac.iscas.xlab.droidfacedog.config;
+
+/**
+ * Created by lisongting on 2017/5/10.
+ */
+
+public class Config {
+
+    public static final String ROS_SERVER_IP = "192.168.1.111";
+
+    public static final String ROS_SERVER_PORT = "9090";
+
+    public static final String RECOGNITION_SERVER_IP = "192.168.0.111";
+
+    public static final String RECOGNITION_SERVER_PORT = "8000";
+
+    public static double RECOG_THRESHOLD ;
+
+    public static boolean ENABLE_MESSAGE_NOTIFICATION;
+
+}
