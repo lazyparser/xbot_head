d904c3e8cc56f5e6ae909150a44f19adca4f1bfb
Lee <501648152@qq.com>
2017-05-10 14:25:06 +0800

    修改全局配置类，对资源文件strings.xml中的内容进行增删,添加设置界面的逻辑部分代码

--- app/src/main/java/cn/ac/iscas/xlab/droidfacedog/config/Config.java
+++ app/src/main/java/cn/ac/iscas/xlab/droidfacedog/config/Config.java
@@ -6,16 +6,24 @@ package cn.ac.iscas.xlab.droidfacedog.config;
 
 public class Config {
 
-    public static final String ROS_SERVER_IP = "192.168.1.111";
+    public static String ROS_SERVER_IP = "192.168.1.111";
 
-    public static final String ROS_SERVER_PORT = "9090";
+    public static String ROS_SERVER_PORT = "9090";
 
-    public static final String RECOGNITION_SERVER_IP = "192.168.0.111";
+    public static String RECOGNITION_SERVER_IP = "192.168.0.111";
 
-    public static final String RECOGNITION_SERVER_PORT = "8000";
+    public static String RECOGNITION_SERVER_PORT = "8000";
 
     public static double RECOG_THRESHOLD ;
 
     public static boolean ENABLE_MESSAGE_NOTIFICATION;
 
+    public static String string() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("人脸识别服务端：" + RECOGNITION_SERVER_IP + ":" + RECOGNITION_SERVER_PORT);
+        sb.append("\nROS服务端：" + ROS_SERVER_IP + ":" + ROS_SERVER_PORT);
+        sb.append("\n阈值:" + RECOG_THRESHOLD);
+        sb.append("\n开启通知：" + ENABLE_MESSAGE_NOTIFICATION);
+        return sb.toString();
+    }
 }
